6b5f35c2687169d2664bcfbd215a4d2bbc6749d7
yhi9839 <yhi9839@gmail.com>
2025-05-22 19:11:14 +0900

    feat : google 로그인

--- src/main/java/cc/backend/member/LoginService.java
+++
@@ -1,9 +0,0 @@
-package cc.backend.member;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class LoginService {
-}
