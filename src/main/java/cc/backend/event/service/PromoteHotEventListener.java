974670dd271cb278da4e8aa7be0d1396148c509b
sweatbukets <imjyh01@gmail.com>
2025-06-27 03:33:59 +0900

    feat: 알림 기능 보완

--- src/main/java/cc/backend/event/service/PromoteHotEventListener.java
+++ src/main/java/cc/backend/event/service/PromoteHotEventListener.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class PromoteHotEventListner {
+public class PromoteHotEventListener {
     private final NoticeService noticeService;
 
     @EventListener
