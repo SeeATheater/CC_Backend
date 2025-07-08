630cddc782d81eb915924ff4eddb8bec928a91bb
sweatbukets <imjyh01@gmail.com>
2025-07-09 02:00:45 +0900

    fix: 사진첩 조회 수정

--- src/main/java/cc/backend/photoAlbum/controller/PhotoAlbumController.java
+++ src/main/java/cc/backend/photoAlbum/controller/PhotoAlbumController.java
@@ -44,8 +44,8 @@ public class PhotoAlbumController {
     @Operation(summary = "등록자 계정의 전체 사진첩 피드 조회 API", description = "등록자의 사진첩 피드를 전체 조회하는 API 입니다.")
     public ApiResponse<List<PhotoAlbumResponseDTO.SinglePhotoAlbumDTO>> getPhotoAlbumList(
             @AuthenticationPrincipal(expression = "member") Member member,
-            @Parameter(description = "사진첩 등록자 계정") Long performerId){
-        return ApiResponse.onSuccess(photoAlbumService.getPhotoAlbumList(member.getId(), performerId));
+            @Parameter(description = "사진첩 등록자 계정") Long memberId){
+        return ApiResponse.onSuccess(photoAlbumService.getPhotoAlbumList(member.getId(), memberId));
     }
 
     @PatchMapping("/{photoAlbumId}")
