5b49b79a7d7457cfca29891f3a968edb243138e8
ddhi7 <tjsanf211323@gmail.com>
2025-09-07 20:25:25 +0900

    fix : 레디스 경로 수정

--- src/main/java/cc/backend/config/RedisConfig.java
+++ src/main/java/cc/backend/config/RedisConfig.java
@@ -13,10 +13,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("${spring.redis.host:localhost}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${spring.redis.port:6379}")
+    @Value("${spring.data.redis.port:6379}")
     private int redisPort;
 
     @Bean
